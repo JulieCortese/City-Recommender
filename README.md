3d6781b836e1e4a3ebbeb6446982a6fe6c011dc8
julie <jcortese@ufl.edu>
2023-07-05 20:31:40 -0400

    added CMakeLists (hopefully it's just mad because this isn't committed)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # City-Recommender
+I'm not sure what to write here, but I'm pretty sure we're supposed to have a readme file. So here it is! :)
--- main.cpp
+++ main.cpp
@@ -1,1 +1,8 @@
 // Created by Julie, 7/5/2023
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "hello" << endl;
+    return 0;
+}

33e499956d794219f63017ce82d23d403e45d2bd
julie <jcortese@ufl.edu>
2023-07-22 22:56:51 -0400

    incredibly incredibly vague layout for the graph class

--- Graph.cpp
+++ Graph.cpp
@@ -1,3 +1,14 @@
 //
 // Created by julie on 7/5/2023.
 //
+/*
+ More specifically this will be an adjacency list since I like adjacency lists and I don't think either of you will
+ object to the idea.
+ */
+
+class Graph{
+private:
+
+public:
+
+};
